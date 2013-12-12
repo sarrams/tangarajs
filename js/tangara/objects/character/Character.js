0f9dcdde9c65806abfbe8f5c5e8bb71ed6c42972
bonpra <benoit.prady@colombbus.org>
2013-12-12 15:35:58 +0100

    - Corrected usage of prototype - Prefixed internal methods with "_" - Added i18n management

--- js/tangara/objects/character/Character.js
+++ js/tangara/objects/character/Character.js
@@ -1,68 +1,75 @@
 define(['jquery','TEnvironment', 'objects/TGraphicalObject'], function($, TEnvironment, TGraphicalObject) {
     Character = function(characterName) {
+        window.console.log("Initializing character");
         TGraphicalObject.call(this);
-        
-        var speed = 8;
-        
-        this.moveForward = function(value) {
-            var element = this.getElement();
-            $(element).animate({"left": "+="+value+"px"}, speed*value, 'linear');
-            return;
-        };
-
-        this.moveBackward = function(value) {
-            var element = this.getElement();
-            $(element).animate({"left": "-="+value+"px"}, speed*value, 'linear');
-            return;
-        };
-        
-        this.moveUpward = function(value) {
-            var element = this.getElement();
-            $(element).animate({"top": "-="+value+"px"}, speed*value, 'linear');
-            return;
-        };
-
-        this.moveDownward = function(value) {
-            var element = this.getElement();
-            $(element).animate({"top": "+="+value+"px"}, speed*value, 'linear');
-            return;
-        };
-        
-        this.stop = function() {
-            var element = this.getElement();
-            $(element).stop();
-            return;
-        }
-        
-        this.loadSkeleton = function(name) {
-            var element = this.getElement();
-            var baseUrl = TEnvironment.instance().getBaseUrl()+"/js/tangara/objects/ressources/character/"+name+"/";
-            var skeletonUrl = baseUrl+"skeleton.json";
-            $.getJSON(skeletonUrl, function(data) {
-                $(element).empty();
-                $.each( data['skeleton']['element'], function( key, val ) {
-                    var image = document.createElement("img");
-                    image.src = baseUrl+val['image'];
-                    image.style.position="absolute";
-                    image.style.left=val['coordinateX']+"px";
-                    image.style.top=val['coordinateY']+"px";
-                    element.appendChild(image);
-                });
-            }).fail(function( jqxhr, textStatus, error ) {
-                throw "unknwon skeleton";
-            });
-        };
-        
-        this.change = function(name) {
-            this.loadSkeleton(name);
-        };
-
+        window.console.log("Character initialized");
+        this.speed = 8;
         if (typeof(characterName)==='undefined')
-            characterName = "boy"
+            characterName = "boy";
+        this._loadSkeleton(characterName);
+    };
+
+    Character.prototype = new TGraphicalObject();
+
+    Character.prototype.className = "Character";
         
-        this.loadSkeleton(characterName);
+    Character.prototype._moveForward = function(value) {
+        var element = this.getElement();
+        $(element).animate({"left": "+="+value+"px"}, this.speed*value, 'linear');
+        return;
+    };
+
+    Character.prototype._moveBackward = function(value) {
+        var element = this.getElement();
+        $(element).animate({"left": "-="+value+"px"}, this.speed*value, 'linear');
+        return;
+    };
+        
+    Character.prototype._moveUpward = function(value) {
+        var element = this.getElement();
+        $(element).animate({"top": "-="+value+"px"}, this.speed*value, 'linear');
+        return;
+    };
+
+    Character.prototype._moveDownward = function(value) {
+        var element = this.getElement();
+        $(element).animate({"top": "+="+value+"px"}, this.speed*value, 'linear');
+        return;
+    };
+        
+    Character.prototype._stop = function() {
+        var element = this.getElement();
+        $(element).stop();
+        return;
+    };
+        
+    Character.prototype._loadSkeleton = function(name) {
+        window.console.log("load Skeleton");
+        var element = this.getElement();
+        var baseImageUrl = this.getResource(name)+"/";
+        var skeletonUrl = baseImageUrl+"skeleton.json";
+        window.console.log("Skeleton URL : "+skeletonUrl);
+        $.getJSON(skeletonUrl, function(data) {
+            $(element).empty();
+            $.each( data['skeleton']['element'], function( key, val ) {
+                var image = document.createElement("img");
+                image.src = baseImageUrl+ val['image'];
+                image.style.position="absolute";
+                image.style.left=val['coordinateX']+"px";
+                image.style.top=val['coordinateY']+"px";
+                element.appendChild(image);
+            });
+        }).fail(function( jqxhr, textStatus, error ) {
+            throw "unknwon skeleton";
+        });
+    };
+        
+    Character.prototype._change = function(name) {
+        this.loadSkeleton(name);
     };
     
+    TEnvironment.instance().internationalize(Character);
+
     return Character;
 });
 

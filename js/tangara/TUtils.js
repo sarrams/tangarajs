0ba5bedb421315f40f0b860fae82e433baeca1be
bonpra <benoit.prady@colombbus.org>
2013-12-13 14:42:46 +0100

    Added a function "format" to format string (like printf)

--- js/tangara/TUtils.js
+++ js/tangara/TUtils.js
@@ -104,6 +104,13 @@ define(['jquery'], function($) {
             }
             return newStr;
         };
+        
+        this.format = function(text) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            return text.replace(/{(\d+)}/g, function(match, number) { 
+                return typeof args[number] !== 'undefined'? args[number]: match;
+            });
+        };
     };
     
     var utilInstance = new TUtils();

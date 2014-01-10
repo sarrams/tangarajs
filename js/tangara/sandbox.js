d9bf7681a170aa3263266ae51ddd7cbf5b22f9ad
bonpra <benoit.prady@colombbus.org>
2014-01-10 18:16:31 +0100

    Added a sandbox iframe

--- js/tangara/sandbox.js
+++ js/tangara/sandbox.js
@@ -11,6 +11,10 @@ require(['jquery'],function($) {
     $(document).ready( function() {
         window.thatObject = Object.create(null);
         window.execute = function(commands) {
+            // sandbox : prevent window and document objects from direct access 
+            // and remove reference to 'this'
+            // not secure though : these objects can still be accessed using functions
+            // e.g. (function(){this.window.alert("hello");})()
             var objectsSandbox = new Function('window', 'document', commands);
             var thisSandbox  = objectsSandbox.bind(window.thatObject);
             thisSandbox({}, {});

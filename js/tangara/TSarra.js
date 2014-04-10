79d43c3406863c21c8469f45c78668eba7d964dc
sarrams <sarramsellek@gmail.com>
2014-04-10 18:51:21 +0200

    Update TSarra.js

--- js/tangara/TSarra.js
+++ js/tangara/TSarra.js
@@ -1,6 +1,6 @@
 define(['jquery'], function($) {
-    function TRegis() {
-        window.console.log("Regis loading");
+    function TSarra() {
+        window.console.log("Sarra loading");
         
         this.simple = function() {
             console.log("méthode simple");
@@ -11,5 +11,5 @@ define(['jquery'], function($) {
         };
     }
     
-    return TRegis;
+    return TSarra;
 });
